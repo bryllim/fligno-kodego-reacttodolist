daf959c1434154db51de2462d45b437d68de227b
bryllim <bryllim@gmail.com>
2022-10-08 10:31:38 +0800

    refactor

--- src/App.js
+++ src/App.js
@@ -1,42 +1,9 @@
 import React from 'react';
 import './App.css';
-import { Button, Card, Form } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
+import FormTodo from './FormTodo';
+import Todo from './Todo';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-function Todo({ todo, index, markTodo, removeTodo }) {
-  return (
-    <div className="todo">
-      <span style={{ textDecoration: todo.isDone ? "line-through": "" }}>{todo.text}</span>
-      <div>
-        <Button variant="outline-success" onClick={()=> markTodo(index)}>✓</Button>{' '}
-        <Button variant="outline-danger" onClick={()=> removeTodo(index)}>✕</Button>
-      </div>
-    </div>
-  )
-}
-
-function FormTodo({ addTodo }) {
-  const [value, setValue] = React.useState("");
-  
-  const handleSubmit = e => {
-    e.preventDefault();
-    if (!value) return;
-    addTodo(value);
-    setValue("");
-  }
-
-  return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group>
-        <Form.Label><b>Add Todo</b></Form.Label>
-        <Form.Control type="text" className="input" value={value} onChange={e => setValue(e.target.value)} placeholder="Add new todo" />
-      </Form.Group>
-      <Button variant='primary mt-2 mb-3' type='submit'>
-        Submit
-      </Button>
-    </Form>
-  )
-}
 
 function App() {
   const [todos, setTodos] = React.useState([
